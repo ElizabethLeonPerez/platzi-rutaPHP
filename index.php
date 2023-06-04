1d43fc7c354ad830d931172a9e41f8ff4a33163c
Elizabeth León <elizabethleon073@gmail.com>
2023-06-04 00:02:40 -0300

    Adhesión de xampp

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "aHHHH";
+
+$nombre = "Carlos";
+$apellido = "Santana";
+
+echo "No, no. Yo me llamo " . $nombre . " " . $apellido . "<br>";
+
+// echo "No, no. Yo me llamo $nombre $apellido <br>";
+
+// echo "El resultado de 4 x 5 es " . (4 * 5) . "<br>";
+
